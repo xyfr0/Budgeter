07071531b0f74f6a7b6a30adbb049fad4093b3b7
tenma2020 <120551033+tenma2020@users.noreply.github.com>
2025-05-15 11:13:33 +0700

    first commit

---
+++ src/main/java/com/mycompany/budgeter/Budgeter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.budgeter;
+
+/**
+ *
+ * @author jabba
+ */
+public class Budgeter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
