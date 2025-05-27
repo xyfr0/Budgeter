357e7d5fa586bbe1333defa21fb7fbbb1aadc5bc
xyfr0 <120551033+xyfr0@users.noreply.github.com>
2025-05-27 16:00:19 +0700

    Update Transaction.java

--- src/main/java/com/mycompany/budgeter/model/Transaction.java
+++ src/main/java/com/mycompany/budgeter/model/Transaction.java
@@ -4,16 +4,74 @@
  */
 package com.mycompany.budgeter.model;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author jabba
  */
 public class Transaction {
-    int id;
-    String category;
-    double amount;
-    String description;
-    
-    
-    
+private int id;
+private double amount;
+private LocalDateTime date;
+private String transactionType;
+private String description;
+private String category;
+
+    public Transaction(int id, double amount, LocalDateTime date, String transactionType, String description, String category) {
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+        this.transactionType = transactionType;
+        this.description = description;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
